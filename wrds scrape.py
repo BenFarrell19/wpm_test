4f58e0e926e366171813eb1e73c61111a76896ed
VALLEJOS, J. Rodrigo <rodrigo.vallejosch@gmail.com>
2019-08-14 01:44:05 -0700

    Moved to wpm/ and wpm/scraper/

--- wrds scrape.py
+++
@@ -1,7 +0,0 @@
-import requests, re
-
-r = requests.get('https://cdn.livechatinc.com/gtt/app.3.8.min.js')
-p = re.compile(r'e\.exports={words:\[(.*?)\]')
-words = p.findall(r.text)
-print(words)
-w = open("wrds_list.txt", "w")
